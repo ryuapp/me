5765ac571005c2ef5abebf13871323775a560003
ryu <114303361+ryuapp@users.noreply.github.com>
2023-12-30 05:44:10 +0900

    Add `--help` and `--version`

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,18 @@ const fs = std.fs;
 const os = std.os;
 const debug = std.debug;
 
+const MIRU_VERSION = "0.1.0";
+const USAGE = "Usage: miru [FILE]...";
+const HELP = USAGE ++ "\n --help\t\tPrint help\n --version\tPrint version";
+
 fn printUsage() void {
-    debug.print("Usage: miru [FILE]...\n", .{});
+    debug.print("{s}", .{USAGE});
+}
+fn printHelp() !void {
+    try std.io.getStdOut().writer().print("{s}", .{HELP});
+}
+fn printVersion() !void {
+    try std.io.getStdOut().writer().print("miru {s}", .{MIRU_VERSION});
 }
 fn printErrorMessage(filename: [:0]u8, err: anyerror) void {
     debug.print("\"{s}\": {any}\n", .{ filename, err });
@@ -20,6 +30,17 @@ pub fn main() !void {
     if (args.len < 2) {
         printUsage();
         os.exit(2);
+    }
+
+    // Arguments
+    for (args[1..args.len]) |filename| {
+        if (std.mem.eql(u8, filename, "--help")) {
+            try printHelp();
+            os.exit(0);
+        } else if (std.mem.eql(u8, filename, "--version")) {
+            try printVersion();
+            os.exit(0);
+        }
     }
 
     for (args[1..args.len]) |filename| {
