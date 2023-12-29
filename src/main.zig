bec477db266a658ee59dd5636b117cacb4b87a0c
ryu <114303361+ryuapp@users.noreply.github.com>
2023-12-30 02:10:34 +0900

    Separated some functions from main()

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,30 @@
 const std = @import("std");
 
+const fs = std.fs;
+const os = std.os;
+const debug = std.debug;
+
+fn printUsage() void {
+    debug.print("Usage: miru [FILE]...\n", .{});
+}
+fn printErrorMessage(filename: [:0]u8, err: anyerror) void {
+    debug.print("\"{s}\": {any}\n", .{ filename, err });
+}
+
 pub fn main() !void {
     const alc = std.heap.page_allocator;
     const args = try std.process.argsAlloc(alc);
-    var buf: [1024]u8 = undefined;
+    var buf: [std.mem.page_size]u8 = undefined;
 
     defer std.process.argsFree(alc, args);
     if (args.len < 2) {
-        std.debug.print("Usage: miru [FILE]...\n", .{});
-        std.os.exit(1);
+        printUsage();
+        os.exit(1);
     }
-    for (args, 0..) |value, i| {
-        if (i == 0) continue;
-        const filename = value;
-        const content = std.fs.cwd().readFile(filename, &buf) catch |err| {
-            std.debug.print("\"{s}\" is not found.\n", .{filename});
-            std.log.debug("{}", .{err});
+
+    for (args[1..args.len]) |filename| {
+        const content = fs.cwd().readFile(filename, &buf) catch |err| {
+            printErrorMessage(filename, err);
             continue;
         };
 
